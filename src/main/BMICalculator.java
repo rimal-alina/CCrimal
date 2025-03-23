9d44717724d54ba8c98e4585fa3386dfc9671fbb
rimal-alina <119261810+rimal-alina@users.noreply.github.com>
2025-03-23 12:12:54 +0100

    Update and rename BMI Rechner/BMI Calculator.java to src/main/BMICalculator.java

--- src/main/BMICalculator.java
+++ src/main/BMICalculator.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class BMICalculatorGUI {
+public class BMICalculator {
     public static void main(String[] args) {
         // Create the frame
         JFrame frame = new JFrame("BMI Rechner");
