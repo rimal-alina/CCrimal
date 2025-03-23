aceaaf8d0932120914ea546867f9512bd6b09f8d
rimal-alina <119261810+rimal-alina@users.noreply.github.com>
2025-03-23 15:51:59 +0100

    Update MyIntegrationTestIT.java

--- src/test/java/MyIntegrationTestIT.java
+++ src/test/java/MyIntegrationTestIT.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-public class MyIntegrationTest 
+public class MyIntegrationTestIT 
 {
    
     @Test
